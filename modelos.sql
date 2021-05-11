1c9bd110af3b29820c196011500937d9b28409d2
nathmelop <nathaliapmelo@hotmail.com>
2021-05-11 08:09:28 -0300

    usando o select

---
+++ modelos.sql
@@ -0,0 +1,75 @@
+select * from cursos;
+
+select nome, descricao,ano  from cursos where ano >='2018'
+order by ano, nome;
+
+select nome, ano from cursos where ano between 2014 and 2015
+order by ano desc, nome asc; 
+
+select nome, descricao, ano from cursos where ano in (2014, 2016) order by ano;
+
+select  nome, totaulas, carga from cursos where carga >35 and totaulas <=30;
+
+select * from cursos where nome ='php';
+
+select * from cursos where nome like '%a';
+
+select * from gafanhotos where nome like '%silva%'; 
+
+
+update cursos set nome ='PáOO' where idcurso='9';
+
+desc cursos;
+
+select  distinct carga from cursos;
+
+select count(*) from cursos;
+
+select count(*) from cursos where carga > 40;
+
+select * from cursos where carga order by carga;
+
+select max(carga) from cursos where ano ='2016';
+
+select min(carga) from cursos where ano;
+
+select sum(carga) from cursos where ano='2016';
+
+select avg(totaulas) from cursos where ano='2016';
+
+
+select * from gafanhotos;
+select nome, sexo from gafanhotos where sexo='f' order by nome;
+
+select * from gafanhotos where nascimento between '2000-01-01' and '2015-12-31';
+
+select * from gafanhotos where profissao='programador' and sexo='M';
+
+select * from gafanhotos where sexo='f' and nacionalidade ='brasil' and nome like 'J%';
+
+select nome, nacionalidade from gafanhotos where sexo='M' and nome like '%silva%'  and peso < 100; 
+
+select max(altura) from gafanhotos where altura and nacionalidade='brasil';
+
+select avg(peso) from gafanhotos;
+
+select min(peso) from gafanhotos where  nacionalidade !='brasil' and sexo='f' and nascimento between '1990-01-01' and '2000-12-31';
+
+select count(*) from gafanhotos where sexo='f' and altura > '1.90';
+
+select carga from cursos group by carga;
+
+select carga, count(nome) from cursos group by carga;
+
+select * from cursos where totaulas =12;
+
+select *from cursos where totaulas = 30;
+
+select ano, count(*) from cursos group by carga having count(*) >5;
+
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos where ano > 2015 group by carga having carga > (select avg(carga) from cursos);
+
+
+select totaulas from cursos order by totaulas;
